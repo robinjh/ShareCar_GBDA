67bb2f285fd09e29e1d2ab372bcbca8b432c2558
Jaehwan Kim <robinjh0314@gmail.com>
2025-05-25 12:30:19 +0900

    feat: create MyInfo component skeleton and base style

---
+++ sharecar/webpage_mypage/src/components/mypage/MyInfo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../../styles/MyInfo.css";
+
+function MyInfo() {
+  return (
+    <div className="myinfo-box">
+      <h3 className="myinfo-title">👤 내 정보</h3>
+      {/* 정보 표시 영역 (추후 구현) */}
+    </div>
+  );
+}
+
+export default MyInfo;
