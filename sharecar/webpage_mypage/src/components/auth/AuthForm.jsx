6c06ac2ea19e4d42e4b4c423690871fa3e6090d6
Yoonhsub <162604517+Yoonhsub@users.noreply.github.com>
2025-05-30 01:02:41 +0900

    feat: Create function call when login progress success

--- sharecar/webpage_mypage/src/components/auth/AuthForm.jsx
+++ sharecar/webpage_mypage/src/components/auth/AuthForm.jsx
@@ -14,7 +14,7 @@ import { UserContext } from "../../UserContext";
 import { setDoc, doc } from "firebase/firestore";
 import "../../styles/AuthForm.css";
 
-function AuthForm() {
+function AuthForm({ onLoginSuccess }) { // onLoginSuccess 값에 따라 AuthForm 공개 결정
   const [name, setName] = useState("");
   const [birth, setBirth] = useState("");
   const [address, setAddress] = useState("");
@@ -73,7 +73,7 @@ function AuthForm() {
     );
   }
 
-  // 로그인/회원가입 처리
+   // 로그인/회원가입 처리
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError(""); setInfo("");
@@ -84,7 +84,13 @@ function AuthForm() {
       try {
         await signInWithEmailAndPassword(auth, email, password);
         setInfo("로그인 성공!");
-      } catch (err) {
+
+        if (onLoginSuccess && typeof onLoginSuccess == 'function') {
+          onLoginSuccess();
+        }
+        // 로그인 버튼 클릭하고 입력 정보에 오류가 없을 시, 마이 페이지로 
+      } 
+      catch (err) {
         if (err.code === "auth/invalid-email") setError("올바른 이메일 형식이 아닙니다.");
         else if (err.code === "auth/user-not-found" || err.code === "auth/wrong-password")
           setError("이메일 또는 비밀번호가 올바르지 않습니다.");
