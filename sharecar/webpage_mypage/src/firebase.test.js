b5e360edee489f6e90e8c84fb6a1ebb6b0004224
Jaehwan Kim <robinjh0314@gmail.com>
2025-06-07 23:02:56 +0900

    Remove all files from main branch

--- sharecar/webpage_mypage/src/firebase.test.js
+++
@@ -1,9 +0,0 @@
-import "@testing-library/jest-dom";
-import { auth, db } from "./firebase";
-
-describe("firebase.js", () => {
-  it("should export auth and db objects", () => {
-    expect(auth).toBeDefined();
-    expect(db).toBeDefined();
-  });
-});
